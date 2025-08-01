d110cacde15e578a7a2524a3a0bc0ac76b87b505
aadip26 <aadipawar4940@gmail.com>
2025-08-01 13:14:12 +0530

    fix: use correct PORT for Render

--- server.js
+++ server.js
@@ -13,5 +13,6 @@ app.get('/api/intern', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
